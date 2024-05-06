4d8c723df78549a6e263616e327c26a1f75a419f
Cris <cckaiser2563@gmail.com>
2024-05-06 11:55:32 +0200

    Setting up window

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module scratch {
+	requires java.desktop;
 }
--- src/scratch/Game.java
+++ src/scratch/Game.java
@@ -1,13 +1,26 @@
 package scratch;
 
-public class Game implements Runnable{
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+public class Game extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	public synchronized void start() {
 		running = true;
@@ -29,5 +42,18 @@ public class Game implements Runnable{
 		while(running) {
 			
 		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Scratch");
+		game.frame.add(game);  //-> Game inherits from Canvas
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 }
