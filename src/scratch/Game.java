cdc5852d3b811680d69bf724fcc7ad627349ff1c
Cris <cckaiser2563@gmail.com>
2024-05-06 16:30:35 +0200

    implemented key Input

--- src/scratch/Game.java
+++ src/scratch/Game.java
@@ -10,6 +10,7 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 import scratch.graphics.Screen;
+import scratch.input.Keyboard;
 
 public class Game extends Canvas implements Runnable{
 	private static final long serialVersionUID = 1L;
@@ -21,9 +22,12 @@ public class Game extends Canvas implements Runnable{
 	
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 	private boolean running = false;
 	
 	private Screen screen;
+	
+	public static String title = "Scratch";
 	
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData(); //converting image to int[]
@@ -33,8 +37,10 @@ public class Game extends Canvas implements Runnable{
 		setPreferredSize(size);
 		
 		this.screen = new Screen(width, height);
+		this.frame = new JFrame();
+		this.key = new Keyboard();
 		
-		frame = new JFrame();
+		addKeyListener(key);
 	}
 	
 	public synchronized void start() {
@@ -55,8 +61,11 @@ public class Game extends Canvas implements Runnable{
 	@Override
 	public void run() {
 		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
 		final double ns = 1000000000.0 / LOGIC_UPDATE_RATE; 
 		double delta = 0.0;
+		int frames = 0;
+		int updates = 0;
 		
 		while(running) {
 			
@@ -66,16 +75,28 @@ public class Game extends Canvas implements Runnable{
 			while(delta >= 1.0) {
 				update();
 				delta = 0.0;
-				System.out.println("update");
+				updates++;
 			}
-			
 			render();
+			frames++;
 			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(title + " | " + String.valueOf(frames) + " fps");
+				frames = 0;
+			}
 		}
+		
+		
 	}
 	
+	int x = 0, y = 0;
 	public void update() {
-		
+		key.update();
+		if (key.up) y--;
+		if (key.down) y++;
+		if (key.left) x--;
+		if (key.right) x++; 
 	}
 	
 	public void render() {
@@ -86,7 +107,7 @@ public class Game extends Canvas implements Runnable{
 		}
 		
 		screen.clear();
-		screen.render();
+		screen.render(x, y);
 		
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
@@ -101,7 +122,6 @@ public class Game extends Canvas implements Runnable{
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("Scratch");
 		game.frame.add(game);  //-> Game inherits from Canvas
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/scratch/graphics/Screen.java
+++ src/scratch/graphics/Screen.java
@@ -1,16 +1,30 @@
 package scratch.graphics;
 
+import java.util.Iterator;
+import java.util.Random;
+
 public class Screen {
 	
 	private int width;
 	private int height;
-	
 	public int[] pixels;
+	
+	public final int MAP_SIZE = 64;
+	public final int MAP_SIZE_MASK = MAP_SIZE -1;
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE]; //->8*8 different tiles
+	
+	private final int tileControl = 4;
+	
+	private Random random = new Random();
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+		
+		for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 	
 	public void clear() {
@@ -19,10 +33,13 @@ public class Screen {
 		}
 	}
 	
-	public void render() {
+	public void render(int xOffset, int yOffset) {
 		for (int y=0; y < height; y++) {
+			int yy = y + yOffset;
 			for (int x = 0; x < width; x++) {
-				pixels[x+y*width] = 0xff00ff;
+				int xx = x + xOffset;
+				int tileIndex = ((xx>>tileControl)&MAP_SIZE_MASK)+((yy>>tileControl)&MAP_SIZE_MASK)*MAP_SIZE; 
+				pixels[x+y*width] = tiles[tileIndex];
 			}
 		}
 	}
